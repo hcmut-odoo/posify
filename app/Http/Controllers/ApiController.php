2a599626401653d259da05a62a24335cafc29805
Võ Qúy Long <long@idctechno.com>
2023-07-02 00:49:16 +0700

    Update mechanism for query data

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,46 +3,102 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\User;
 use App\Models\Store;
 
+use Illuminate\Database\Eloquent\Model;
+
 class ApiController extends Controller
 {
+    public function resourceList(Request $request, string $modelClass)
+    {
+        // Retrieve the JSON payload from the request
+        $data = $request->json()->all();
+
+        // Extract the parameters from the payload
+        $filters = $data['filter'] ?? [];
+        $display = $data['display'] ?? [];
+        $sort = $data['sort'] ?? [];
+        $date = $data['date'] ?? [];
+        $limit = $data['limit'] ?? 10;
+        $page = $data['page'] ?? 1;
+
+        // Query the resource based on the filters
+        $query = $modelClass::query();
+
+        // Apply filters
+        foreach ($filters as $field => $filter) {
+            $operator = $filter['operator'];
+            $value = $filter['value'];
+
+            if ($operator === 'eq') {
+                $query->where($field, $value);
+            } elseif ($operator === 'like') {
+                $query->where($field, 'like', "%$value%");
+            } elseif ($operator === 'lt') {
+                $query->where($field, '<', $value);
+            } elseif ($operator === 'lteq') {
+                $query->where($field, '<=', $value);
+            } elseif ($operator === 'neq') {
+                $query->where($field, '!=', $value);
+            }
+        }
+
+        // Apply date range
+        if ($date) {
+            if ($date['start']) {
+                $start = Carbon::parse($date['start']);
+                $query->where('updated_at', '>=', $start);
+            }
+            if ($date['end']) {
+                $end = Carbon::parse($date['end']);
+                $query->where('updated_at', '<=', $end);
+            }
+        }
+
+        // Selected fields
+        if ($display && count($display) > 0) {
+            if (!in_array('id', $display)) {
+                array_unshift($display, 'id');
+            }
+            $query->select($display);
+        }
+
+        // Apply sorting
+        foreach ($sort as $field => $order) {
+            $query->orderBy($field, $order);
+        }
+
+        // Apply pagination
+        $query->limit($limit)->offset(($page - 1) * $limit);
+
+        // Retrieve the resources
+        $resources = $query->get();
+
+        // Return the resource list as JSON response
+        return response()->json($resources);
+    }
+
     public function products(Request $request)
     {
-        // Number of items per page, default is 10
-        $perPage = $request->input('per_page', 10);
-        $products = Product::pagination($perPage);
-
-        return response()->json($products);
+        return $this->resourceList($request, Product::class);
     }
 
     public function users(Request $request)
     {
-        // Number of items per page, default is 10
-        $perPage = $request->input('per_page', 10);
-        $products = User::pagination($perPage);
-
-        return response()->json($products);
+        return $this->resourceList($request, User::class);
     }
 
     public function categories(Request $request)
     {
-        // Number of items per page, default is 10
-        $perPage = $request->input('per_page', 10);
-        $products = Category::pagination($perPage);
-
-        return response()->json($products);
+        return $this->resourceList($request, Category::class);
     }
 
     public function stores(Request $request)
     {
-        // Number of items per page, default is 10
-        $perPage = $request->input('per_page', 10);
-        $products = Store::pagination($perPage);
-
-        return response()->json($products);
+        return $this->resourceList($request, Store::class);
     }
 }
