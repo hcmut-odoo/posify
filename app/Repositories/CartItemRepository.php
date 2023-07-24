8d7b445414d85419a468641328929319c59dbff0
Võ Qúy Long <long@idctechno.com>
2023-07-24 10:18:52 +0700

    add create and search method

--- app/Repositories/CartItemRepository.php
+++ app/Repositories/CartItemRepository.php
@@ -11,7 +11,7 @@ class CartItemRepository
 {
     public function get($id)
     {
-        return CartItem::where('id', $id)->get();
+        return CartItem::find($id);
     }
 
     public function remove($id)
@@ -39,5 +39,32 @@ class CartItemRepository
         return false;
     }
 
+    public function create($productId, $cartId, $size, $note, $quantity)
+    {
+        return CartItem::create([
+            'product_id' => $productId,
+            'cart_id' => $cartId,
+            'size' => $size,
+            'note' => $note,
+            'quantity' => $quantity
+        ]);
+    }
 
+    public function search($criteria)
+    {
+        return CartItem::query()
+            ->when(isset($criteria['cart_id']), function ($query) use ($criteria) {
+                $query->where('cart_id', $criteria['cart_id']);
+            })
+            ->when(isset($criteria['product_id']), function ($query) use ($criteria) {
+                $query->where('product_id', $criteria['product_id']);
+            })
+            ->when(isset($criteria['size']), function ($query) use ($criteria) {
+                $query->where('size', $criteria['size']);
+            })
+            ->when(isset($criteria['note']), function ($query) use ($criteria) {
+                $query->where('note', $criteria['note'] == false ? null : $criteria['note']);
+            })
+            ->first();
+    }
 }
