5bf97049effec00882e94d34766739280cfc6cf6
Võ Qúy Long <long@idctechno.com>
2023-07-25 16:13:42 +0700

    group routes

--- routes/web.php
+++ routes/web.php
@@ -52,90 +52,112 @@ Route::middleware('auth')->group(function () {
     Route::get('/orders/{id}', [OrderController::class, 'orderDetail'])->name('order.detail');
 
     // Admin routes
-    Route::middleware('admin')->group(function () {
-        Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::middleware('admin')->prefix('/admin')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 
         // product routes
-        Route::get('/admin/products', [ProductController::class, 'index'])->name('admin.product.list');
-        Route::get('/admin/products/delete', [ProductController::class, 'delete'])->name('admin.product.delete');
-        Route::get('/admin/products/edit', [ProductController::class, 'update'])->name('admin.product.update');
-        Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin.product.create');
-        Route::get('/admin/products/detail', [ProductController::class, 'detail'])->name('admin.product.detail');
+        Route::prefix('/products')->group(function () {
+            Route::get('/', [ProductController::class, 'index'])->name('admin.product.list');
+            Route::get('/delete', [ProductController::class, 'delete'])->name('admin.product.delete');
+            Route::get('/edit', [ProductController::class, 'update'])->name('admin.product.update');
+            Route::get('/create', [ProductController::class, 'create'])->name('admin.product.create');
+            Route::get('/detail', [ProductController::class, 'detail'])->name('admin.product.detail');
+        });
 
         // category routes
-        Route::get('/admin/categories', [CategoryController::class, 'index'])->name('admin.category.list');
-        Route::get('/admin/categories/delete', [CategoryController::class, 'delete'])->name('admin.category.delete');
-        Route::get('/admin/categories/edit', [CategoryController::class, 'update'])->name('admin.category.update');
-        Route::get('/admin/categories/create', [CategoryController::class, 'create'])->name('admin.category.create');
-        Route::get('/admin/categories/detail', [CategoryController::class, 'detail'])->name('admin.category.detail');
+        Route::prefix('/categories')->group(function () {
+            Route::get('/', [CategoryController::class, 'index'])->name('admin.category.list');
+            Route::get('/delete', [CategoryController::class, 'delete'])->name('admin.category.delete');
+            Route::get('/edit', [CategoryController::class, 'update'])->name('admin.category.update');
+            Route::get('/create', [CategoryController::class, 'create'])->name('admin.category.create');
+            Route::get('/detail', [CategoryController::class, 'detail'])->name('admin.category.detail');
+        });
 
         // user routes
-        Route::get('/admin/users', [UserController::class, 'index'])->name('admin.user.list');
-        Route::get('/admin/users/delete', [UserController::class, 'delete'])->name('admin.user.delete');
-        Route::get('/admin/users/edit', [UserController::class, 'update'])->name('admin.user.update');
-        Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.user.create');
-        Route::get('/admin/users/detail', [UserController::class, 'detail'])->name('admin.user.detail');
+        Route::prefix('/users')->group(function () {
+            Route::get('/', [UserController::class, 'index'])->name('admin.user.list');
+            Route::get('/delete', [UserController::class, 'delete'])->name('admin.user.delete');
+            Route::get('/edit', [UserController::class, 'update'])->name('admin.user.update');
+            Route::get('/create', [UserController::class, 'create'])->name('admin.user.create');
+            Route::get('/detail', [UserController::class, 'detail'])->name('admin.user.detail');
+        });
 
         // store routes
-        Route::get('/admin/stores', [StoreController::class, 'index'])->name('admin.store.list');
-        Route::get('/admin/stores/delete', [StoreController::class, 'delete'])->name('admin.store.delete');
-        Route::get('/admin/stores/edit', [StoreController::class, 'update'])->name('admin.store.update');
-        Route::get('/admin/stores/create', [StoreController::class, 'create'])->name('admin.store.create');
-        Route::get('/admin/stores/detail', [StoreController::class, 'detail'])->name('admin.store.detail');
+        Route::prefix('/stores')->group(function () {
+            Route::get('/', [StoreController::class, 'index'])->name('admin.store.list');
+            Route::get('/delete', [StoreController::class, 'delete'])->name('admin.store.delete');
+            Route::get('/edit', [StoreController::class, 'update'])->name('admin.store.update');
+            Route::get('/create', [StoreController::class, 'create'])->name('admin.store.create');
+            Route::get('/detail', [StoreController::class, 'detail'])->name('admin.store.detail');
+        });
 
         // order routes
-        Route::get('/admin/orders', [OrderController::class, 'index'])->name('admin.order.list');
-        Route::post('/admin/orders/delete', [OrderController::class, 'delete'])->name('admin.order.delete');
-        Route::post('/admin/orders/accept', [OrderController::class, 'acceptOrder'])->name('admin.order.accept');
-        Route::post('/admin/orders/reject', [OrderController::class, 'rejectOrder'])->name('admin.order.reject');
-        Route::get('/admin/orders/detail/{id}', [OrderController::class, 'detail'])->name('admin.order.detail');
-        Route::get('/admin/orders/accepted', [OrderController::class, 'acceptedOrderIndex'])->name('admin.order.accepted');
-        Route::get('/admin/orders/rejected', [OrderController::class, 'rejectedOrderIndex'])->name('admin.order.rejected');
-        Route::get('/admin/orders/accepted/detail/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.accepted.detail');
-        Route::post('/admin/orders/accepted/delete/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.accepted.delete');
-        Route::get('/admin/orders/rejected/detail/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.rejected.detail');
-        Route::post('/admin/orders/rejected/delete/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.rejected.delete');
+        Route::prefix('/orders')->group(function () {
+            Route::get('/', [OrderController::class, 'index'])->name('admin.order.list');
+            Route::post('/delete', [OrderController::class, 'delete'])->name('admin.order.delete');
+            Route::post('/accept', [OrderController::class, 'acceptOrder'])->name('admin.order.accept');
+            Route::post('/reject', [OrderController::class, 'rejectOrder'])->name('admin.order.reject');
+            Route::get('/detail/{id}', [OrderController::class, 'detail'])->name('admin.order.detail');
+            Route::get('/accepted', [OrderController::class, 'acceptedOrderIndex'])->name('admin.order.accepted');
+            Route::get('/rejected', [OrderController::class, 'rejectedOrderIndex'])->name('admin.order.rejected');
+            Route::get('/accepted/detail/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.accepted.detail');
+            Route::post('/accepted/delete/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.accepted.delete');
+            Route::get('/rejected/detail/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.rejected.detail');
+            Route::post('/rejected/delete/{id}', [OrderController::class, 'acceptedOrderDetail'])->name('admin.order.rejected.delete');
+        });
 
         // invoice routes
-        Route::get('/admin/invoices', [InvoiceController::class, 'index'])->name('admin.invoice.list');
-        Route::post('/admin/invoices/delete', [InvoiceController::class, 'delete'])->name('admin.invoice.delete');
-        Route::get('/admin/invoices/detail/{id}', [InvoiceController::class, 'invoiceDetail'])->name('admin.invoice.detail');
-        Route::get('/admin/invoices/print/{id}', [InvoiceController::class, 'invoiceForm'])->name('admin.invoice.form');
+        Route::prefix('/orders')->group(function () {
+            Route::get('/', [InvoiceController::class, 'index'])->name('admin.invoice.list');
+            Route::post('/delete', [InvoiceController::class, 'delete'])->name('admin.invoice.delete');
+            Route::get('/detail/{id}', [InvoiceController::class, 'invoiceDetail'])->name('admin.invoice.detail');
+            Route::get('/print/{id}', [InvoiceController::class, 'invoiceForm'])->name('admin.invoice.form');
+        });
 
         // Api-key routes
-        Route::get('/admin/api-key', [ApiKeyController::class, 'index'])->name('admin.api.key.list');
-        Route::get('/admin/api-key/delete', [ApiKeyController::class, 'delete'])->name('admin.api.key.delete');
-        Route::get('/admin/api-key/edit', [ApiKeyController::class, 'update'])->name('admin.api.key.update');
-        Route::get('/admin/api-key/create', [ApiKeyController::class, 'create'])->name('admin.api.key.create');
-        Route::get('/admin/api-key/detail', [ApiKeyController::class, 'detail'])->name('admin.api.key.detail');
+        Route::prefix('/api-key')->group(function () {
+            Route::get('/', [ApiKeyController::class, 'index'])->name('admin.api.key.list');
+            Route::get('/delete', [ApiKeyController::class, 'delete'])->name('admin.api.key.delete');
+            Route::get('/edit', [ApiKeyController::class, 'update'])->name('admin.api.key.update');
+            Route::get('/create', [ApiKeyController::class, 'create'])->name('admin.api.key.create');
+            Route::get('/detail', [ApiKeyController::class, 'detail'])->name('admin.api.key.detail');
+        });
 
         // User-group routes
-        Route::get('/admin/user-group', [UserGroupController::class, 'index'])->name('admin.user.group.list');
-        Route::get('/admin/user-group/delete', [UserGroupController::class, 'delete'])->name('admin.user.group.delete');
-        Route::get('/admin/user-group/edit', [UserGroupController::class, 'update'])->name('admin.user.group.update');
-        Route::get('/admin/user-group/create', [UserGroupController::class, 'create'])->name('admin.user.group.create');
-        Route::get('/admin/user-group/detail', [UserGroupController::class, 'detail'])->name('admin.user.group.detail');
+        Route::prefix('/user-group')->group(function() {
+            Route::get('/', [UserGroupController::class, 'index'])->name('admin.user.group.list');
+            Route::get('/delete', [UserGroupController::class, 'delete'])->name('admin.user.group.delete');
+            Route::get('/edit', [UserGroupController::class, 'update'])->name('admin.user.group.update');
+            Route::get('/create', [UserGroupController::class, 'create'])->name('admin.user.group.create');
+            Route::get('/detail', [UserGroupController::class, 'detail'])->name('admin.user.group.detail');
+        });
 
         // User-role routes
-        Route::get('/admin/user-role', [UserRoleController::class, 'index'])->name('admin.user.role.list');
-        Route::get('/admin/user-role/delete', [UserRoleController::class, 'delete'])->name('admin.user.role.delete');
-        Route::get('/admin/user-role/edit', [UserRoleController::class, 'update'])->name('admin.user.role.update');
-        Route::get('/admin/user-role/create', [UserRoleController::class, 'create'])->name('admin.user.role.create');
-        Route::get('/admin/user-role/detail', [UserRoleController::class, 'detail'])->name('admin.user.role.detail');
+        Route::prefix('/user-role')->group(function() {
+            Route::get('/', [UserRoleController::class, 'index'])->name('admin.user.role.list');
+            Route::get('/delete', [UserRoleController::class, 'delete'])->name('admin.user.role.delete');
+            Route::get('/edit', [UserRoleController::class, 'update'])->name('admin.user.role.update');
+            Route::get('/create', [UserRoleController::class, 'create'])->name('admin.user.role.create');
+            Route::get('/detail', [UserRoleController::class, 'detail'])->name('admin.user.role.detail');
+        });
 
         // Role-group routes
-        Route::get('/admin/role-group', [RoleGroupController::class, 'index'])->name('admin.role.group.list');
-        Route::get('/admin/role-group/delete', [RoleGroupController::class, 'delete'])->name('admin.role.group.delete');
-        Route::get('/admin/role-group/edit', [RoleGroupController::class, 'update'])->name('admin.role.group.update');
-        Route::get('/admin/role-group/create', [RoleGroupController::class, 'create'])->name('admin.role.group.create');
-        Route::get('/admin/role-group/detail', [RoleGroupController::class, 'detail'])->name('admin.role.group.detail');
+        Route::prefix('/role-group')->group(function() {
+            Route::get('/', [RoleGroupController::class, 'index'])->name('admin.role.group.list');
+            Route::get('/delete', [RoleGroupController::class, 'delete'])->name('admin.role.group.delete');
+            Route::get('/edit', [RoleGroupController::class, 'update'])->name('admin.role.group.update');
+            Route::get('/create', [RoleGroupController::class, 'create'])->name('admin.role.group.create');
+            Route::get('/detail', [RoleGroupController::class, 'detail'])->name('admin.role.group.detail');
+        });
 
         // Action routes
-        Route::get('/admin/action', [ActionController::class, 'index'])->name('admin.action.list');
-        Route::get('/admin/action/delete', [ActionController::class, 'delete'])->name('admin.action.delete');
-        Route::get('/admin/action/edit', [ActionController::class, 'update'])->name('admin.action.update');
-        Route::get('/admin/action/create', [ActionController::class, 'create'])->name('admin.action.create');
-        Route::get('/admin/action/detail', [ActionController::class, 'detail'])->name('admin.action.detail');
+        Route::prefix('/action')->group(function() {
+            Route::get('/', [ActionController::class, 'index'])->name('admin.action.list');
+            Route::get('/delete', [ActionController::class, 'delete'])->name('admin.action.delete');
+            Route::get('/edit', [ActionController::class, 'update'])->name('admin.action.update');
+            Route::get('/create', [ActionController::class, 'create'])->name('admin.action.create');
+            Route::get('/detail', [ActionController::class, 'detail'])->name('admin.action.detail');
+        });
     });
 });
 
