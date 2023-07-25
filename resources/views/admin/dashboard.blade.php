8c6f2ddaad17040adf7d477070c2392f2e73a06b
Võ Qúy Long <long@idctechno.com>
2023-07-25 16:00:50 +0700

    add invoice into navbar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,19 +7,19 @@
                 <div class="card card-stats card-warning">
                 <div class="card-body">
                     <div class="row">
-                    <div class="col-5">
-                        <div class="icon-big text-center">
-                        <i class="fa fa-users"></i>
+                        <div class="col-5">
+                            <div class="icon-big text-center">
+                                <i class="fa fa-users"></i>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-7 d-flex align-items-center">
-                        <div class="numbers">
-                        <a href="/admin/users">
-                            <p class="card-category">Thành viên</p>
-                        </a>
-                        <h4 class="card-title">{{ $users }}</h4>
+                        <div class="col-7 d-flex align-items-center">
+                            <div class="numbers">
+                                <a href="/admin/users">
+                                    <p class="card-category">Thành viên</p>
+                                </a>
+                                <h4 class="card-title">{{ $users }}</h4>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
                 </div>
@@ -28,63 +28,63 @@
                 <div class="card card-stats card-success">
                 <div class="card-body">
                     <div class="row">
-                    <div class="col-5">
-                        <div class="icon-big text-center">
-                        <i class="fas fa-chart-bar"></i>
+                        <div class="col-5">
+                            <div class="icon-big text-center">
+                            <i class="fas fa-chart-bar"></i>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-7 d-flex align-items-center">
-                        <div class="numbers">
-                        <a href="/admin/orders/accepted">
-                            <p class="card-category">Doanh thu</p>
-                        </a>
-                        <h4 class="card-title">{{ number_format($income, 0, ',', '.') }} VNĐ</h4>
+                        <div class="col-7 d-flex align-items-center">
+                            <div class="numbers">
+                                <a href="/admin/orders/accepted">
+                                    <p class="card-category">Doanh thu</p>
+                                </a>
+                                <h4 class="card-title">{{ number_format($income, 0, ',', '.') }} VNĐ</h4>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-stats card-danger">
-                <div class="card-body">
-                    <div class="row">
-                    <div class="col-5">
-                        <div class="icon-big text-center">
-                        <i class="far fa-newspaper"></i>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-5">
+                                <div class="icon-big text-center">
+                                    <i class="far fa-newspaper"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 d-flex align-items-center">
+                                <div class="numbers">
+                                    <a href="/admin/products">
+                                        <p class="card-category">Sản phẩm</p>
+                                    </a>
+                                    <h4 class="card-title">{{ $products }}</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-7 d-flex align-items-center">
-                        <div class="numbers">
-                        <a href="/admin/products">
-                            <p class="card-category">Sản phẩm</p>
-                        </a>
-                        <h4 class="card-title">{{ $products }}</h4>
-                        </div>
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-stats card-primary">
-                <div class="card-body">
-                    <div class="row">
-                    <div class="col-5">
-                        <div class="icon-big text-center">
-                        <i class="far fa-check-circle"></i>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-5">
+                                <div class="icon-big text-center">
+                                <i class="far fa-check-circle"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 d-flex align-items-center">
+                                <div class="numbers">
+                                    <a href="/admin/orders/accepted">
+                                        <p class="card-category">Đã bán</p>
+                                    </a>
+                                    <h4 class="card-title">{{ $orders }}</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-7 d-flex align-items-center">
-                        <div class="numbers">
-                        <a href="/admin/orders/accepted">
-                            <p class="card-category">Đã bán</p>
-                        </a>
-                        <h4 class="card-title">{{ $orders }}</h4>
-                        </div>
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
@@ -100,7 +100,7 @@
                 <header class="panel-heading"></header>
                 <div class="panel-body">
                     <p class="col-lg-12 text-center">
-                        Copyright (C) {{ date('Y') }} - <a href="http://kevinrodriguez.io/">kevinrodriguez.io</a>
+                        Copyright (C) {{ date('Y') }} - <a href="https://github.com/Blue-Pheasant">Blue-Pheasant</a>
                     </p>
                 </div>
             </section>
