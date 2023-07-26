7c64b8d6e04e9d3c96453ecbd7fb63087a774b57
Võ Qúy Long <long@idctechno.com>
2023-07-26 22:19:07 +0700

    api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,154 +2,399 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
+use App\Models\CartItem;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\User;
 use App\Models\Store;
+use App\Exceptions\InvalidApiKeyException;
+use App\Exceptions\InvalidParameterException;
+use App\Exceptions\NotFoundException;
+use App\Exceptions\DeleteFailedException;
+use App\Exceptions\UpdateFailedException;
+use App\Services\CartService;
+use App\Services\OrderService;
+use App\Services\UserService;
+use App\Services\ProductService;
+use App\Services\CategoryService;
+use App\Services\StoreService;
+use App\Services\ApiService;
+use App\Services\InvoiceService;
+use App\Http\Requests\QueryRequest;
+use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Requests\CreateProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 
 class ApiController extends Controller
 {
+    private $cartService;
+    private $orderService;
+    private $userService;
+    private $productService;
+    private $categoryService;
+    private $storeService;
+    private $apiService;
+    private $invoiceService;
 
-    public function checkConnection(Request $request)
+    public function __construct(
+        CartService $cartService,
+        OrderService $orderService,
+        UserService $userService,
+        ProductService $productService,
+        CategoryService $categoryService,
+        StoreService $storeService,
+        ApiService $apiService,
+        InvoiceService $invoiceService
+    )
     {
-        $apiKey = $request->header('X-API-Key');
+        $this->cartService = $cartService;
+        $this->orderService = $orderService;
+        $this->userService = $userService;
+        $this->productService = $productService;
+        $this->categoryService = $categoryService;
+        $this->storeService = $storeService;
+        $this->apiService = $apiService;
+        $this->invoiceService = $invoiceService;
+    }
 
-        // Check if the API key is valid
-        if ($apiKey) {
-            return response()->json(['message' => 'Connection successful'], 200);
+    private function errorResponse(\Exception $exception, int $statusCode = 400, string $data = null): JsonResponse
+    {
+        $message = 'Failed';
+        $responseData = $data ?? $exception->getMessage();
+        $responseCode = $statusCode ?? $exception->getCode();
+
+        if ($exception instanceof InvalidParameterException) {
+            $message = 'Validation parameters errors';
+        } elseif ($exception instanceof NotFoundException) {
+            $message = 'Not found errors';
+        } elseif ($exception instanceof InvalidApiKeyException) {
+            $message = 'Validation api key errors';
+        } elseif ($exception instanceof DeleteFailedException) {
+            $message = 'Delete record failed';
+        } elseif ($exception instanceof UpdateFailedException) {
+            $message = 'Update record failed';
         } else {
-            return response()->json(['message' => 'API key missing or invalid'], 400);
+            $message = 'Runtime errors';
+            $data = "An error occurred: " . $responseData;
         }
+
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $responseData,
+        ], $responseCode);
     }
 
-    public function resourceList(Request $request, string $modelClass)
+
+    private function successResponse($data, string $message = "success", int $statusCode = 200): JsonResponse
     {
-        // Retrieve the JSON payload from the request
-        $data = $request->json()->all();
-
-        // Extract the parameters from the payload
-        $filters = $data['filter'] ?? [];
-        $display = $data['display'] ?? [];
-        $sort = $data['sort'] ?? [];
-        $date = $data['date'] ?? [];
-        $limit = $data['limit'] ?? 10;
-        $page = $data['page'] ?? 1;
-
-        // Query the resource based on the filters
-        $query = $modelClass::query();
-
-        // Apply filters
-        foreach ($filters as $field => $filter) {
-            $operator = $filter['operator'];
-            $value = $filter['value'];
-
-            if ($operator === 'eq') {
-                $query->where($field, $value);
-            } elseif ($operator === 'like') {
-                $query->where($field, 'like', "%$value%");
-            } elseif ($operator === 'lt') {
-                $query->where($field, '<', $value);
-            } elseif ($operator === 'lteq') {
-                $query->where($field, '<=', $value);
-            } elseif ($operator === 'gt') {
-                $query->where($field, '.', $value);
-            } elseif ($operator === 'gteq') {
-                $query->where($field, '>=', $value);
-            } elseif ($operator === 'neq') {
-                $query->where($field, '!=', $value);
-            }
-        }
-
-        // Apply date range
-        if ($date) {
-            if ($date['start']) {
-                $start = Carbon::parse($date['start']);
-                $query->where('updated_at', '>=', $start);
-            }
-            if ($date['end']) {
-                $end = Carbon::parse($date['end']);
-                $query->where('updated_at', '<=', $end);
-            }
-        }
-
-        // Selected fields
-        if ($display && count($display) > 0) {
-            if (!in_array('id', $display)) {
-                array_unshift($display, 'id');
-            }
-            $query->select($display);
-        }
-
-        // Apply sorting
-        foreach ($sort as $field => $order) {
-            $query->orderBy($field, $order);
-        }
-
-        // Apply pagination
-        $query->limit($limit)->offset(($page - 1) * $limit);
-
-        // Retrieve the resources
-        $resources = $query->get();
-
-        // Return the resource list as JSON response
-        return response()->json($resources);
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
     }
 
-    public function products(Request $request)
+    public function resourceList(QueryRequest $request, string $modelClass) : JsonResponse
+    {
+        $data = $request->validated();
+        $resources = $this->apiService->query($data, $modelClass);
+
+        return $this->successResponse($resources);
+    }
+
+    public function checkConnection(Request $request) : JsonResponse
+    {
+        try {
+            $apiKey = $request->header("X-API-Key");
+            $this->apiService->checkConnection($apiKey);
+            return $this->successResponse("Connection successful");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function orderItems(QueryRequest $request) : JsonResponse
+    {
+        return $this->resourceList($request, OrderItem::class);
+    }
+
+    public function cartItems(QueryRequest $request) : JsonResponse
+    {
+        return $this->resourceList($request, CartItem::class);
+    }
+
+    public function products(QueryRequest $request) : JsonResponse
     {
         return $this->resourceList($request, Product::class);
     }
 
-    public function users(Request $request)
+    public function getProduct(Request $request, $id) : JsonResponse
     {
-        return $this->resourceList($request, User::class);
+        try {
+            $product = $this->productService->findById($id);
+            return $this->successResponse($product);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
     }
 
-    public function categories(Request $request)
+    public function updateProduct(UpdateProductRequest $request, $id) : JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $product = $this->productService->updateProduct($data);
+            return response()->json($product);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function createProduct(CreateProductRequest $request) : JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $product = $this->productService->createProduct($data);
+            return $this->successResponse($product);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function deleteProduct(Request $request, $id) : JsonResponse
+    {
+        try {
+            $this->productService->deleteProduct($id);
+            return $this->successResponse("Product with ID $id has been deleted");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function categories(QueryRequest $request) : JsonResponse
     {
         return $this->resourceList($request, Category::class);
     }
 
-    public function stores(Request $request)
+    public function createCategory(CreateCategoryRequest $request) : JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $category = $this->categoryService->createCategory($data);
+            return $this->successResponse($category);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function getCategory(Request $request, $id) : JsonResponse
+    {
+        try {
+            $category = $this->categoryService->findById($id);
+            return response()->json($category);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function updateCategory(UpdateCategoryRequest $request) : JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $category = $this->categoryService->updateCategory($data);
+            return $this->successResponse($category);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function deleteCategory(Request $request, $id) : JsonResponse
+    {
+        try {
+            $this->categoryService->deleteCategory($id);
+            return $this->successResponse("Category with ID $id has been deleted");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function createUser(CreateUserRequest $request) : JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $user = $this->userService->createUser($data);
+            return $this->successResponse($user);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function users(Request $request) : JsonResponse
+    {
+        return $this->resourceList($request, User::class);
+    }
+
+    public function getUser(Request $request, $id) : JsonResponse
+    {
+        try {
+            $user = $this->userService->findById($id);
+            return $this->successResponse($user);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function updateUser(UpdateUserRequest $request) : JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $user = $this->userService->updateUser($data);
+            return $this->successResponse($user);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function deleteUser(Request $request, $id) : JsonResponse
+    {
+        try {
+            $this->userService->deleteUser($id);
+            return $this->successResponse("User has ID $id deleted successfully");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function stores(Request $request) : JsonResponse
     {
         return $this->resourceList($request, Store::class);
     }
 
-    public function getProduct(Request $request, $id)
+    public function getStore(Request $request, $id) : JsonResponse
     {
-        $product = Product::find($id);
-        if (!$product) {
-            return response()->json(['error' => 'Item not found'], 404);
+        try {
+            $store = $this->storeService->findById($id);
+            return $this->successResponse($store);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
         }
-        return response()->json($product);
     }
 
-    public function getCategory(Request $request, $id)
+    public function updateStore(Request $request, $id) : JsonResponse
     {
-        $category = Category::find($id);
-        if (!$category) {
-            return response()->json(['error' => 'Item not found'], 404);
+        try {
+            $data = $request->validated();
+            $store = $this->storeService->updateStore($data);
+            return $this->successResponse($store);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
         }
-        return response()->json($category);
     }
 
-    public function getUser(Request $request, $id)
+    public function createStore(Request $request, $id) : JsonResponse
     {
-        $user = User::find($id);
-        if (!$user) {
-            return response()->json(['error' => 'Item not found'], 404);
+        try {
+            $data = $request->validated();
+            $store = $this->storeService->createStore($data);
+            return $this->successResponse($store);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
         }
-        return response()->json($user);
     }
 
-    public function getStore(Request $request, $id)
+    public function deleteStore(Request $request, $id) : JsonResponse
     {
-        $store = Store::find($id);
-        if (!$store) {
-            return response()->json(['error' => 'Item not found'], 404);
+        try {
+            $this->storeService->deleteStore($id);
+            return $this->successResponse("Store has ID $id deleted successfully");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
         }
-        return response()->json($store);
+    }
+
+    public function getCartItems(Request $request, $id) : JsonResponse
+    {
+        try {
+            $cartItems = $this->cartService->getCartItems($id);
+            return $this->successResponse($cartItems);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function orders(Request $request) : JsonResponse
+    {
+        return $this->resourceList($request, Order::class);
+    }
+
+    public function getOrder(Request $request, $id) : JsonResponse
+    {
+        try {
+            $order = $this->orderService->findById($id);
+            return $this->successResponse($order);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function acceptOrder(Request $request, $id) : JsonResponse
+    {
+        try {
+            $this->invoiceService->createInvoice($id);
+            return $this->successResponse("Order has ID $id processed successfully");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function rejectOrder(Request $request, $id) : JsonResponse
+    {
+        try {
+            $this->orderService->rejectOrder($id);
+            return $this->successResponse("Order has ID $id rejected successfully");
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function rejectedOrders(Request $request) : JsonResponse
+    {
+        try {
+            $perPage = $request->query('per_page');
+            $page = $request->query('page');
+            $rejectedOrders = $this->orderService->pagination(['status' => 'cancel'], $perPage, $page);
+            return $this->successResponse($rejectedOrders);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function acceptedOrders(Request $request, $id) : JsonResponse
+    {
+        try {
+            $perPage = $request->query('per_page');
+            $page = $request->query('page');
+            $acceptedOrders = $this->orderService->pagination(['status' => 'done'], $perPage, $page);
+            return $this->successResponse($acceptedOrders);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
+    }
+
+    public function getOrderItems(Request $request, $id) : JsonResponse
+    {
+        try {
+            $rejectedOrders = $this->orderService->getOrderItems($id);
+            return $this->successResponse($rejectedOrders);
+        } catch (\Exception $e) {
+            return $this->errorResponse($e);
+        }
     }
 }
