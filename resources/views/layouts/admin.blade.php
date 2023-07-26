e8f72520858c2d3d06ef9a5f1f4042ee70f0f2c2
Võ Qúy Long <long@idctechno.com>
2023-07-26 17:43:00 +0700

    add admin css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" href="{{ asset('/css/admin/profile.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/admin/dashboard.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/admin/toast.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/admin/general.css') }}">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
