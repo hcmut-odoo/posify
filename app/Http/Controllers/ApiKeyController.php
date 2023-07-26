ad62c331e0da801affd76d006a071277bdb52b80
Võ Qúy Long <long@idctechno.com>
2023-07-26 22:18:59 +0700

    stamp controller

--- app/Http/Controllers/ApiKeyController.php
+++ app/Http/Controllers/ApiKeyController.php
@@ -5,8 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+use App\Services\ApiService;
+use Illuminate\Support\Facades\Session;
+
 class ApiKeyController extends Controller
 {
+    private $apiService;
+
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
     /**
      * Generate and assign an API key to the user.
      *
@@ -26,5 +36,47 @@ class ApiKeyController extends Controller
         $user->save();
 
         return response()->json(['api_key' => $apiKey]);
+    }
+
+    public function index(Request $request)
+    {
+        $apiKeyList = $this->apiService->getAll();
+
+        return view('/admin/apis', [
+            'apis' => $apiKeyList
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        if ($request->getMethod() === 'POST') {
+            $apiKey = $this->apiService->generate($request->user()->id);
+            if ($apiKey) {
+                Session::flash('createApiKeyMessage', 'Api key was created successfully!');
+            } else {
+                Session::flash('createApiKeyMessage', 'Failed to create api key.');
+            }
+            return redirect()->back();
+        }
+
+        return view('/admin/apis/create');
+    }
+
+    public function update(Request $request)
+    {
+        $apiKeyList = $this->apiService->getAll();
+
+        return view('/admin/apis', [
+            'apis' => $apiKeyList
+        ]);
+    }
+
+    public function read(Request $request)
+    {
+        $apiKeyList = $this->apiService->getAll();
+
+        return view('/admin/apis', [
+            'apis' => $apiKeyList
+        ]);
     }
 }
