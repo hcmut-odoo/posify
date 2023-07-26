4c5b9fb669a77cf4cf68367b635da3dde9155e47
Võ Qúy Long <long@idctechno.com>
2023-07-26 22:17:46 +0700

    stamp api middleware

--- app/Http/Middleware/ApiKeyAuthentication.php
+++ app/Http/Middleware/ApiKeyAuthentication.php
@@ -3,28 +3,45 @@
 namespace App\Http\Middleware;
 
 use App\Models\User;
+use App\Services\ApiService;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class ApiKeyAuthentication
 {
+    private $apiService;
+
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
     public function handle(Request $request, Closure $next)
     {
         $apiKey = $request->header('X-API-Key');
-
         if (empty($apiKey)) {
             return response()->json(['error' => 'API key is missing.'], 401);
         }
 
-        $user = User::where('api_key', $apiKey)->first();
-
-        if (!$user) {
+        $apiKeyRecord = $this->apiService->getByKey($apiKey);
+        if (!$apiKeyRecord) {
             return response()->json(['error' => 'Invalid API key.'], 401);
         }
 
-        $request->setUserResolver(function () use ($user) {
-            return $user;
-        });
+        $isExpired = $this->apiService->checkExpired($apiKey);
+        if (!$isExpired) {
+            return response()->json(['error' => 'API key was expired.'], 401);
+        }
+
+        $controller = class_basename(Route::current()->controller);
+        $method = Route::current()->getActionMethod();
+
+        $hasPermisstion = $this->apiService->checkPermission($apiKeyRecord, $controller, $method);
+
+        if (!$hasPermisstion) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
 
         return $next($request);
     }
