7efeec4a49cefde4b0f59eca44ef4b0e94c7492d
Võ Qúy Long <long@idctechno.com>
2023-08-07 21:59:22 +0700

    update route api

--- routes/api.php
+++ routes/api.php
@@ -30,9 +30,12 @@ Route::prefix('/user')->group(function () {
     Route::get('/find/{id}', [ApiController::class, 'getUserById'])->name('api.user.find.id');
     Route::get('/find', [ApiController::class, 'getUser'])->name('api.user.find');
     Route::get('/list', [ApiController::class, 'users'])->name('api.user.list');
-    Route::post('/update/{id}', [ApiController::class, 'updateUser'])->name('api.user.update');
-    Route::post('/delete/{id}', [ApiController::class, 'deleteUser'])->name('api.user.delete');
-    Route::get('/address/{id}', [ApiController::class, 'getUserAddress'])->name('api.user.address');
+    Route::post('/update', [ApiController::class, 'updateUser'])->name('api.user.update');
+    Route::post('/delete', [ApiController::class, 'deleteUser'])->name('api.user.delete');
+    Route::get('/address', [ApiController::class, 'getUserAddress'])->name('api.user.address');
+    Route::post('/update/{id}', [ApiController::class, 'updateUserById'])->name('api.user.update.id');
+    Route::post('/delete/{id}', [ApiController::class, 'deleteUserById'])->name('api.user.delete.id');
+    Route::get('/address/{id}', [ApiController::class, 'getUserAddressById'])->name('api.user.address.id');
 });
 
 
@@ -43,15 +46,18 @@ Route::prefix('/address')->group(function () {
 
 Route::prefix('/product')->group(function () {
     Route::post('/create', [ApiController::class, 'createProduct'])->name('api.product.create');
-    Route::get('/find/{id}', [ApiController::class, 'getProduct'])->name('api.product.find');
     Route::get('/list', [ApiController::class, 'products'])->name('api.product.list');
-    Route::post('/update/{id}', [ApiController::class, 'updateProduct'])->name('api.product.update');
-    Route::post('/delete/{id}', [ApiController::class, 'deleteProduct'])->name('api.product.delete');
+    Route::get('/find', [ApiController::class, 'getProduct'])->name('api.product.find');
+    Route::post('/update', [ApiController::class, 'updateProduct'])->name('api.product.update');
+    Route::post('/delete', [ApiController::class, 'deleteProduct'])->name('api.product.delete');
+    Route::post('/update/{id}', [ApiController::class, 'updateProductById'])->name('api.product.update.id');
+    Route::post('/delete/{id}', [ApiController::class, 'deleteProductById'])->name('api.product.delete.id');
+    Route::get('/find/{id}', [ApiController::class, 'getProductById'])->name('api.product.find.id');
 });
 
 Route::prefix('/category')->group(function () {
     Route::post('/create', [ApiController::class, 'createCategory'])->name('api.category.create');
-    Route::get('/find/{id}', [ApiController::class, 'getCategory'])->name('api.category.find.v1');
+    Route::get('/find/{id}', [ApiController::class, 'getCategoryById'])->name('api.category.find.v1');
     Route::get('/find', [ApiController::class, 'getCategory'])->name('api.category.find.v2');
     Route::get('/list', [ApiController::class, 'categories'])->name('api.category.list');
     Route::post('/update/{id}', [ApiController::class, 'updateCategory'])->name('api.category.update.v1');
@@ -62,24 +68,30 @@ Route::prefix('/category')->group(function () {
 
 Route::prefix('/store')->group(function () {
     Route::post('/create', [ApiController::class, 'createStore'])->name('api.store.create');
-    Route::get('/find/{id}', [ApiController::class, 'getStore'])->name('api.store.find');;
+    Route::get('/find', [ApiController::class, 'getStore'])->name('api.store.find');;
     Route::get('/list', [ApiController::class, 'stores'])->name('api.store.list');
-    Route::get('/update/{id}', [ApiController::class, 'updateStore'])->name('api.store.update');
-    Route::get('/delete/{id}', [ApiController::class, 'deleteStore'])->name('api.store.delete');
+    Route::get('/update', [ApiController::class, 'updateStore'])->name('api.store.update');
+    Route::get('/delete', [ApiController::class, 'deleteStore'])->name('api.store.delete');
+    Route::get('/find/{id}', [ApiController::class, 'getStoreById'])->name('api.store.find');;
+    Route::get('/update/{id}', [ApiController::class, 'updateStoreById'])->name('api.store.update');
+    Route::get('/delete/{id}', [ApiController::class, 'deleteStoreById'])->name('api.store.delete');
 });
 
 Route::prefix('/order')->group(function () {
     Route::get('/find/{id}', [ApiController::class, 'getOrder'])->name('api.order.find');;
     Route::get('/list', [ApiController::class, 'orders'])->name('api.order.list');
-    Route::get('/accept/{id}', [ApiController::class, 'acceptOrder'])->name('api.order.accept');
-    Route::get('/reject/{id}', [ApiController::class, 'deleteOrder'])->name('api.order.reject');
+    Route::get('/accept', [ApiController::class, 'acceptOrder'])->name('api.order.accept');
+    Route::get('/reject', [ApiController::class, 'rejectOrder'])->name('api.order.reject');
     Route::get('/accepted/list', [ApiController::class, 'acceptedOrders'])->name('api.order.accepted.list');
     Route::get('/rejected/list', [ApiController::class, 'rejectedOrders'])->name('api.order.rejected.list');
+    Route::get('/accept/{id}', [ApiController::class, 'acceptOrderById'])->name('api.order.accept.id');
+    Route::get('/reject/{id}', [ApiController::class, 'rejectOrderById'])->name('api.order.reject.id');
 });
 
 Route::prefix('/invoice')->group(function () {
-    Route::get('/find/{id}', [ApiController::class, 'getInvoice'])->name('api.invoice.find');;
+    Route::get('/find', [ApiController::class, 'getInvoice'])->name('api.invoice.find');
     Route::get('/list', [ApiController::class, 'invoices'])->name('api.invoice.list');
+    Route::get('/find/{id}', [ApiController::class, 'getInvoiceById'])->name('api.invoice.find.id');
 });
 
 Route::middleware('auth')->group(function () {
