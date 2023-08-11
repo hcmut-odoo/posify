5351d0fa0b6589f6cd948ea3596a526bc71c7125
Võ Qúy Long <long@idctechno.com>
2023-08-11 21:17:36 +0700

    Add product variant model and migration

---
+++ app/Repositories/ProductVariantRepository.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\ProductVariant;
+
+class ProductVariantRepository
+{
+    public function get($id)
+    {
+        return ProductVariant::find($id);
+    }
+
+    public function getForUpdate($id)
+    {
+        return ProductVariant::where('id', $id)->lockForUpdate()->first();
+    }
+
+    public function findByProductId($id)
+    {
+        return ProductVariant::where('product_id', $id)->get();
+    }
+
+    public function remove($id)
+    {
+        return ProductVariant::where('id', $id)->delete();
+    }
+
+    public function pagination($perPage, $page)
+    {
+        return ProductVariant::paginate($perPage, ['*'], 'page', $page);
+    }
+
+    public function findByProductIdAndSize($productId, $size)
+    {
+        return ProductVariant::where('product_id', $productId)
+            ->where('size', $size)
+            ->first();
+    }
+
+    public function findBySizeAndColor($productId, $size, $color)
+    {
+        return ProductVariant::where('product_id', $productId)
+            ->where('size', $size)
+            ->where('color', $color)
+            ->first();
+    }
+
+    public function create($productId, $size, $extendPrice, $color, $stockQuantity)
+    {
+        return ProductVariant::create([
+            'product_id' => $productId,
+            'size' => $size,
+            'extend_price' => $extendPrice,
+            'stock_qty' => $stockQuantity,
+            'color' => $color ?? 'none'
+        ]);
+    }
+
+    public function update($data)
+    {
+        $fields = ['product_id', 'size', 'color', 'stock_qty', 'extend_price'];
+        $updateData = [];
+
+        foreach ($fields as $field) {
+            if (isset($data[$field])) {
+                $updateData[$field] = $data[$field];
+            }
+        }
+
+        if (!empty($updateData)) {
+            return DB::table('product_variants')
+                ->where('id', $data['id'])
+                ->update($updateData);
+        }
+
+        return false;
+    }
+}
