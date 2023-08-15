f73d8d974c67636cc24acdc213438d847cb6a406
Võ Qúy Long <long@idctechno.com>
2023-08-15 09:30:54 +0700

    Fix update user profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,42 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\ProfileService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class ProfileController extends Controller
 {
-    private $profileService;
+    private $userService;
 
-    public function __construct(ProfileService $profileService)
+    public function __construct(UserService $userService)
     {
         $this->middleware('auth');
-        $this->profileService = $profileService;
+        $this->userService = $userService;
     }
 
     public function profile(Request $request)
     {
-        $updateSuccess = false;
         $user = Auth::user();
 
-        if ($request->isMethod('POST')) {
-            $this->validate($request, [
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
-                'password' => 'nullable|string|min:8|confirmed',
-            ]);
+        try {
+            if ($request->isMethod('POST')) {
+                $data = $this->validate($request, [
+                    'name' => ['required', 'string', 'max:255'],
+                    'address' => ['required', 'string', 'max:255'],
+                    'phone_number' => ['required', 'regex:/^0[1-9][0-9]{7,14}$/'],
+                    'email' => ['required', 'string', 'email', 'max:255'],
+                ]);
 
-            $updateSuccess = $this->profileService->updateProfile($user, $request);
+                $data["id"] = $user->id;
+                $this->userService->updateUser($data);
 
-            if ($updateSuccess) {
-                $updateSuccess = true;
+                Session::flash('message', "Profile was updated successfully");
             }
+        } catch (\Exception $e) {
+            Session::flash('message', "An error occurred: " . $e->getMessage());
         }
 
         return view('profile', [
-            'user' => $user,
-            'updateSuccess' => $updateSuccess,
+            'user' => $user
         ]);
     }
+
 }
