1c6d10d1f2f7e2c576e8a1a7994cff0ce315655a
Võ Qúy Long <long@idctechno.com>
2023-08-17 10:41:34 +0700

    Use for pos's users

---
+++ database/migrations/2023_08_14_084938_create_user_access_key_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAccessKeyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_access_keys', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('api_key_id');
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('api_key_id')->references('id')->on('api_keys');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        Schema::dropIfExists('user_access_keys');
+
+        // Re-enable foreign key constraints
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+}
