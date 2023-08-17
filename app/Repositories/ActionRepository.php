1fa6a2f8056c039704a33d7ac0450c91b20910aa
Võ Qúy Long <long@idctechno.com>
2023-08-17 11:00:23 +0700

    Update method search to return array

--- app/Repositories/ActionRepository.php
+++ app/Repositories/ActionRepository.php
@@ -40,12 +40,23 @@ class ActionRepository
             ->when(isset($criteria['id']), function ($query) use ($criteria) {
                 $query->where('id', $criteria['id']);
             })
+            ->when(isset($criteria['resource_id']), function ($query) use ($criteria) {
+                $query->where('resource_id', $criteria['resource_id']);
+            })
+            ->when(isset($criteria['permission_id']), function ($query) use ($criteria) {
+                $query->where('permission_id', $criteria['permission_id']);
+            })
             ->when(isset($criteria['controller']), function ($query) use ($criteria) {
                 $query->where('controller', $criteria['controller']);
             })
             ->when(isset($criteria['method']), function ($query) use ($criteria) {
                 $query->where('method', $criteria['method']);
             })
-            ->first($selected);
+            ->get($selected ?? ['*']);
+    }
+
+    public function findByResourceId($resourceId)
+    {
+        return Action::where('resource_id', $resourceId)->get()->toArray();
     }
 }
