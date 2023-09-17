15b75ca840e9fc05d0389f97f47df78464f68d54
Võ Qúy Long <long@idctechno.com>
2023-09-17 10:29:17 +0700

    One order can has many order items

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Order;
 use App\Models\User;
+use App\Models\OrderItem;
 
 class OrderFactory extends Factory
 {
@@ -19,15 +20,26 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
-            'user_id' => User::factory(),
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
             'status' => $this->faker->randomElement(['processing', 'done', 'cancel']),
             'order_transaction' => Str::uuid()->toString(),
-            'delivery_note' => $this->faker->optional()->text(100),
+            'delivery_note' => $this->faker->optional(0.7)->text(100),
             'delivery_phone' => $this->faker->phoneNumber(),
             'delivery_address' => $this->faker->address(),
             'delivery_name' => $this->faker->name(),
             'total' => $this->faker->randomFloat(2, 30000, 200000), // Adjust the range as needed
             'payment_mode_id' => $this->faker->randomElement([1, 2, 3]),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Order $order) {
+            OrderItem::factory()
+                ->count(random_int(1, 5))
+                ->create(['order_id' => $order->id]);
+        });
     }
 }
