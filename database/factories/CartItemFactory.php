58f5edd52c7463219972c53c9f2fb1aab9cb8a77
Võ Qúy Long <long@idctechno.com>
2023-09-17 10:30:06 +0700

    An cart item craete randome product variant

--- database/factories/CartItemFactory.php
+++ database/factories/CartItemFactory.php
@@ -1,11 +1,10 @@
 <?php
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\CartItem;
 use App\Models\Cart;
-use App\Models\Product;
+use App\Models\CartItem;
 use App\Models\ProductVariant;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CartItemFactory extends Factory
 {
@@ -13,10 +12,13 @@ class CartItemFactory extends Factory
 
     public function definition()
     {
+        $productVariant = ProductVariant::factory()->create();
+        $productId = $productVariant->product_id;
+
         return [
             'cart_id' => Cart::factory(), // Establish the relationship to Cart factory
-            'product_id' => Product::factory(), // Establish the relationship to Product factory
-            'product_variant_id' => ProductVariant::factory(), // Establish the relationship to ProductVariant factory
+            'product_id' => $productId, // Use the associated Product's ID
+            'product_variant_id' => $productVariant->id,
             'quantity' => $this->faker->numberBetween(1, 5),
             'stamp' => $this->faker->boolean(),
             'note' => $this->faker->optional()->text(100),
