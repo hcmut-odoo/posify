561845464f7b151224c0fb22760eee613ac961c7
Võ Qúy Long <long10a102001@gmail.com>
2023-09-27 22:43:19 +0700

    Validate update variant requests

---
+++ app/Http/Requests/UpdateProductVariantRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateProductVariantRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'id' => 'sometimes|required|exists:product_variants,id',
+            'product_id' => 'sometimes|required|exists:products,id',
+            'size' => 'nullable|string',
+            'color' => 'nullable|string',
+            'extend_price' => 'sometimes|required|numeric|min:1',
+            'stock_qty' => 'sometimes|required|numeric|min:1',
+            'description' => 'nullable|string',
+            'image_url' => 'nullable|string',
+            'category_id' => 'sometimes|numeric|min:1|exists:categories,id'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
